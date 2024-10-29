7b4a0dc855264e112ee438a90ad0df5de20cc329
SagallaBrian <l.brian@mufin.co.kr>
2024-10-29 17:45:49 +0300

    Start

---
+++ src/app/fly-over/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h4>Fly Over ...</h4>
+      <p>
+        1. The evil general <br />
+        2. The GOOD and wise leader <br />
+        3. The departed and good father <br />
+        4. The corrupted robin hood <br />
+        5. Character <br />
+        6. The lies <br />
+        7. The people <br />
+        a. Deceived <br />
+        b. The untold <br />
+        c. The forced but relatively good <br />
+        d. The willing forced (lovers of the sin) <br />
+      </p>
+    </div>
+  );
+};
+
+export default page;
